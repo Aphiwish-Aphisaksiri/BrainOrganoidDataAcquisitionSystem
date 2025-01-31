37909e63d3845a516f7cddd04a58a639d0843513
Aphiwish Aphisaksiri <aphiwish.api@student.mahidol.edu>
2025-01-31 16:15:03 +0700

    add: serial testing to test dysconnectivity

---
+++ serial_testing.py
@@ -0,0 +1,28 @@
+import serial
+import time
+
+def main():
+    port = 'COM4'  # Replace with your serial port
+    baudrate = 1460000  # Replace with your baud rate
+
+    try:
+        ser = serial.Serial(port, baudrate, timeout=1)
+        print(f"Connected to {port}")
+
+        while True:
+            if ser.in_waiting > 0:
+                serial_data = ser.read(ser.in_waiting)
+                print(serial_data)
+            time.sleep(0.0001)  # Sleep for a short time to avoid high CPU usage
+
+    except serial.SerialException as e:
+        print(f"Serial error: {e}")
+    except KeyboardInterrupt:
+        print("Exiting...")
+    finally:
+        if ser.is_open:
+            ser.close()
+            print(f"Disconnected from {port}")
+
+if __name__ == "__main__":
+    main()
