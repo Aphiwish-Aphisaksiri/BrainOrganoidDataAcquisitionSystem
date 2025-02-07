3c32413e083ff7749b24b1ec98525ad40697b8a8
Aphiwish Aphisaksiri <aphiwish.api@student.mahidol.edu>
2025-02-07 14:31:23 +0700

    [x]feat: added filter
    [x] untested

--- ui/ui_dataProc.py
+++ ui/ui_dataProc.py
@@ -3,13 +3,14 @@ import time
 import numpy as np
 import dearpygui.dearpygui as dpg
 from util.abstractthread import abstractthread
+from util.config import CHANNELS_NUMBER, CONVERTED_RAW_DATA_BUFFER_SIZE, HIGH_PASS_FILTER, LOW_PASS_FILTER, NOTCH_FILTER, GAIN
 
 class UiDataProc(abstractthread):
     def __init__(self, filterThread):
         super().__init__()
         self.__filterThread = filterThread
-        self.setThreadFrequency(10000)
-        self.__channelsNumber = 8
+        self.setThreadFrequency(30)
+        self.__channelsNumber = CHANNELS_NUMBER
 
         #TODO: Implement get filter range and gain from filterThread
 
@@ -18,21 +19,26 @@ class UiDataProc(abstractthread):
         with dpg.window(label="Data processing UI", width=400, height=300):
             dpg.add_text("Filter Range")
             with dpg.group(horizontal=True):
-                dpg.add_input_text(default_value="0.25",
+                dpg.add_input_text(default_value=HIGH_PASS_FILTER,
                                     width=100,
                                     callback=self.setHighPassFilter)
                 dpg.add_text(" - ")
-                dpg.add_input_text(default_value="3000",
+                dpg.add_input_text(default_value=LOW_PASS_FILTER,
                                     width=100,
                                     callback=self.setLowPassFilter)
                 dpg.add_text("Hz")
+            dpg.add_text("Notch Filter")
+            dpg.add_input_text(default_value=NOTCH_FILTER,
+                                width=100,
+                                callback=self.setNotchFilter)
 
             dpg.add_spacer(height=5)
             dpg.add_text("Gain")
-            dpg.add_slider_int(label="Gain", 
-                               min_value=0.5, 
+            dpg.add_input_float(min_value=0.5, 
                                max_value=2000.0, 
-                               default_value=1000,
+                               default_value=GAIN,
+                               width=250,
+                               step=1.0,
                                callback=self.setGain)
             
     def assignFilter(self, target):
@@ -47,6 +53,9 @@ class UiDataProc(abstractthread):
 
     def setLowPassFilter(self):
         #TODO: Implement setLowPassFilter
+        pass
+
+    def setNotchFilter(self):
         pass
 
     def setGain(self):
