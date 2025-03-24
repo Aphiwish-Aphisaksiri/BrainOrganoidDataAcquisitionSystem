7601550a36ad12da48f1fb38c3ac44f4a177c21e
Aphiwish Aphisaksiri <aphiwish.api@student.mahidol.edu>
2025-03-24 16:50:50 +0700

    Add: feedback box on settings UI

--- brainorganoid/ui/ui_settings.py
+++ brainorganoid/ui/ui_settings.py
@@ -9,10 +9,16 @@ class UiSettings:
         """Render the settings UI."""
         with dpg.window(label="Settings", tag="tag_window_settings", width=400, height=300):
             dpg.add_text("|Application Control|")
-            dpg.add_button(label="Exit", callback=self.exitApplication, tag="btn_Exit", width=126)
+            dpg.add_button(label="Exit", callback=self.exitApplication, tag="btn_Exit", width=140)
+            dpg.add_input_text(tag="ui_setting_feedback",
+                    width=260,
+                    height=50,
+                    readonly=True,
+                    multiline=True,
+                    default_value="Application Running")
 
     def exitApplication(self):
         """Callback to exit the application."""
-        dpg.set_value("daq_setting_feedback", "Exiting application...")
+        dpg.set_value("ui_setting_feedback", "Exiting application...")
         self.__app.stopApp()  # Call the App's stop method to terminate all processes and threads
         dpg.stop_dearpygui()  # Stop the Dear PyGui event loop
